da4114f5a98c76c4f786e8bd7d6c40c2260d49bf
Elag <elangindra15@gmail.com>
2019-08-08 19:08:50 +0700

    redesign argon template

---
+++ resources/views/admin/component/sidebar-menu.blade.php
@@ -0,0 +1,107 @@
+<!-- Navigation -->
+<ul class="navbar-nav">
+    <li class="nav-item  class=" active" ">
+        <a class=" nav-link active " href=" ./index.html"> <i class="ni ni-tv-2 text-primary"></i> Dashboard
+        </a>
+    </li>
+    <li class="nav-item">
+        <div class="dropdown" style="width:100%">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><i class="ni ni-book-bookmark text-blue"></i>
+                Bibliobigrafi</a>
+            <div class="dropdown-menu" style="left: 15px;">
+                <a href="#" class="dropdown-item">Daftar Bibliobigrafi</a>
+                <a href="#" class="dropdown-item">Tambah Bibliobigrafi</a>
+                <a href="#" class="dropdown-item">Daftar Eksemplar</a>
+                <a href="#" class="dropdown-item">Eksemplar Keluar</a>
+                <a href="#" class="dropdown-item">Cetak Label</a>
+                <a href="#" class="dropdown-item">Eksport Data</a>
+                <a href="#" class="dropdown-item">Import Data</a>
+            </div>
+        </div>
+        </a>
+    </li>
+    <li class="nav-item">
+        <div class="dropdown" style="width:100%">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                <i class="ni ni-compass-04 text-orange"></i> Sirkulasi
+            </a>
+            <div class="dropdown-menu" style="left: 15px;">
+                <a href="#" class="dropdown-item">Mulai Transaksi</a>
+                <a href="#" class="dropdown-item">Pengembalian Buku</a>
+                <a href="#" class="dropdown-item">Histori Peminjaman</a>
+                <a href="#" class="dropdown-item">Daftar Keterlambatan</a>
+            </div>
+    </li>
+    <li class="nav-item">
+        <div class="dropdown" style="width:100%">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                <i class="ni ni-single-02 text-yellow"></i> Keanggotaan
+            </a>
+            <div class="dropdown-menu" style="left: 15px;">
+                <a href="#" class="dropdown-item">Lihat Daftar Anggota</a>
+                <a href="#" class="dropdown-item">Tambah Anggota</a>
+                <a href="#" class="dropdown-item">Tipe Keanggotaan</a>
+                <a href="#" class="dropdown-item">Cetak Kartu Anggota</a>
+                <a href="#" class="dropdown-item">Eksport Data</a>
+                <a href="#" class="dropdown-item">Import Data</a>
+            </div>
+    </li>
+    <li class="nav-item">
+        <div class="dropdown" style="width:100%">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                <i class="ni ni-archive-2 text-pink"></i> Master File
+            </a>
+            <div class="dropdown-menu" style="left: 15px;">
+                <a href="#" class="dropdown-item">GMD</a>
+                <a href="#" class="dropdown-item">Penerbit</a>
+                <a href="#" class="dropdown-item">Buku</a>
+                <a href="#" class="dropdown-item">Pengarang</a>
+                <a href="#" class="dropdown-item">Kota</a>
+                <a href="#" class="dropdown-item">Klasifikasi</a>
+                <a href="#" class="dropdown-item">Bahasa</a>
+            </div>
+        </div>
+    </li>
+    <li class="nav-item">
+        <div class="dropdown" style="width:100%">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                <i class="ni ni-chart-bar-32 text-info"></i> Laporan
+            </a>
+            <div class="dropdown-menu" style="left: 15px;">
+                <a href="#" class="dropdown-item">Statistik Koleksi Buku</a>
+                <a href="#" class="dropdown-item">Laporan Peminjaman</a>
+                <a href="#" class="dropdown-item">Laporan Anggota</a>
+                <a href="#" class="dropdown-item">Rekapitulasi</a>
+                <a href="#" class="dropdown-item">Statistik Pengunjung</a>
+                <a href="#" class="dropdown-item">Statistik Pengunjung (Per Hari)</a>
+            </div>
+        </div>
+    </li>
+    {{-- <li class="nav-item">
+        <a class="nav-link" href="./examples/register.html">
+            <i class="ni ni-circle-08 text-pink"></i> Register
+        </a>
+    </li> --}}
+</ul>
+<!-- Divider -->
+<hr class="my-3">
+<!-- Heading -->
+<h6 class="navbar-heading text-muted">Lainnya   </h6>
+<!-- Navigation -->
+<ul class="navbar-nav mb-md-3">
+    <li class="nav-item">
+        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
+            <i class="ni ni-align-left-2"></i> Daftar Pengunjung
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
+            <i class="ni ni-collection"></i> Laporan Denda
+        </a>
+    </li>
+    {{-- <li class="nav-item">
+        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">
+            <i class="ni ni-ui-04"></i> Components
+        </a>
+    </li> --}}
+</ul>
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -87,65 +87,7 @@
                 </div>
               </div>
             </form>
-            <!-- Navigation -->
-            <ul class="navbar-nav">
-              <li class="nav-item  class=" active" ">
-              <a class=" nav-link active " href=" ./index.html"> <i class="ni ni-tv-2 text-primary"></i> Dashboard
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="./examples/icons.html">
-                  <i class="ni ni-planet text-blue"></i> Icons
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="./examples/maps.html">
-                  <i class="ni ni-pin-3 text-orange"></i> Maps
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="./examples/profile.html">
-                  <i class="ni ni-single-02 text-yellow"></i> User profile
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="./examples/tables.html">
-                  <i class="ni ni-bullet-list-67 text-red"></i> Tables
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./examples/login.html">
-                  <i class="ni ni-key-25 text-info"></i> Login
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./examples/register.html">
-                  <i class="ni ni-circle-08 text-pink"></i> Register
-                </a>
-              </li>
-            </ul>
-            <!-- Divider -->
-            <hr class="my-3">
-            <!-- Heading -->
-            <h6 class="navbar-heading text-muted">Documentation</h6>
-            <!-- Navigation -->
-            <ul class="navbar-nav mb-md-3">
-              <li class="nav-item">
-                <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
-                  <i class="ni ni-spaceship"></i> Getting started
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
-                  <i class="ni ni-palette"></i> Foundation
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">
-                  <i class="ni ni-ui-04"></i> Components
-                </a>
-              </li>
-            </ul>
+              @include('admin.component.sidebar-menu')
           </div>
         </div>
       </nav>
