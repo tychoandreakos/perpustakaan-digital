970347dfee0014b02f0be6573a3530f3684789f8
Elag <elangindra15@gmail.com>
2019-08-11 20:31:03 +0700

    add seach

--- public/js/app.js
+++ public/js/app.js
@@ -6850,6 +6850,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
--- resources/js/components/user/Search.vue
+++ resources/js/components/user/Search.vue
@@ -4,7 +4,8 @@
         @input="changed" @click-input="clickInput" @click-button="clickButton" @selected="selected" @enter="enter"
         @key-up="keyUp" @key-down="keyDown" @key-right="keyRight" @clear="clear" @escape="escape"
         :show-autocomplete="true" :autofocus="false" :suggestions="suggestions" name="customName"
-        placeholder="Masukkan satu atau lebih kata kunci dari judul, pengarang, atau subyek " type="google"></vue-instant>
+        placeholder="Masukkan satu atau lebih kata kunci dari judul, pengarang, atau subyek " type="google">
+    </vue-instant>
 </template>
 
 <script>
@@ -19,40 +20,42 @@
             }
 
         },
-               methods: {
-            clickInput: function() {
+        methods: {
+            clickInput: function () {
                 this.selectedEvent = 'click input'
             },
-            clickButton: function() {
+            clickButton: function () {
                 this.selectedEvent = 'click button'
             },
-            selected: function() {
+            selected: function () {
                 this.selectedEvent = 'selection changed'
             },
-            enter: function() {
+            enter: function () {
                 this.selectedEvent = 'enter'
             },
-            keyUp: function() {
+            keyUp: function () {
                 this.selectedEvent = 'keyup pressed'
             },
-            keyDown: function() {
+            keyDown: function () {
                 this.selectedEvent = 'keyDown pressed'
             },
-            keyRight: function() {
+            keyRight: function () {
                 this.selectedEvent = 'keyRight pressed'
             },
-            clear: function() {
+            clear: function () {
                 this.selectedEvent = 'clear input'
             },
-            escape: function() {
+            escape: function () {
                 this.selectedEvent = 'escape'
             },
-            changed: function() {
+            changed: function () {
                 var that = this
                 this.suggestions = []
-                axios.get('https://api.themoviedb.org/3/search/movie?api_key=342d3061b70d2747a1e159ae9a7e9a36&query=' + this.value)
-                    .then(function(response) {
-                        response.data.results.forEach(function(a) {
+                axios.get(
+                        'https://api.themoviedb.org/3/search/movie?api_key=342d3061b70d2747a1e159ae9a7e9a36&query=' +
+                        this.value)
+                    .then(function (response) {
+                        response.data.results.forEach(function (a) {
                             that.suggestions.push(a)
                         })
                     })
