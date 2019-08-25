9bf25993ef2235e4b702884e766664e7bc3a279d
Elag <elangindra15@gmail.com>
2019-08-25 23:24:32 +0700

    buku tamu edited

--- app/Tamu.php
+++ app/Tamu.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Tamu extends Model
 {
     protected $table = 'buku_tamu';
     protected $guarded = [];
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return $this->attributes['updated_at'] = Carbon::parse($value)->diffForHumans();
+    }
 }
