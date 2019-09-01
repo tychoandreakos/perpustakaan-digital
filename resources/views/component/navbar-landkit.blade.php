fce9f3906580b7b3b5c2b825b57e4384954f8eba
Elag <elangindra15@gmail.com>
2019-09-02 05:14:07 +0700

    program has 90% completed status

--- resources/views/component/navbar-landkit.blade.php
+++ resources/views/component/navbar-landkit.blade.php
@@ -57,88 +57,7 @@
                          </a>
                          <div class="dropdown-menu dropdown-menu-md" aria-labelledby="navbarDocumentation">
                              <div class="list-group list-group-flush">
-                                 <a class="list-group-item" href="docs/index.html">
 
-                                     <!-- Icon -->
-                                     <div class="icon icon-sm text-primary">
-                                         <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1"
-                                             xmlns="http://www.w3.org/2000/svg"
-                                             xmlns:xlink="http://www.w3.org/1999/xlink">
-                                             <!-- Generator: Sketch 52.2 (67145) - http://www.bohemiancoding.com/sketch -->
-                                             <title>Stockholm-icons / General / Clipboard</title>
-                                             <desc>Created with Sketch.</desc>
-                                             <g id="Stockholm-icons-/-General-/-Clipboard" stroke="none"
-                                                 stroke-width="1" fill="none" fill-rule="evenodd">
-                                                 <rect id="bound" x="0" y="0" width="24" height="24"></rect>
-                                                 <path
-                                                     d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z"
-                                                     id="Combined-Shape" fill="#335EEA" opacity="0.3"></path>
-                                                 <path
-                                                     d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z"
-                                                     id="Combined-Shape" fill="#335EEA"></path>
-                                                 <rect id="Rectangle-152" fill="#335EEA" opacity="0.3" x="7" y="10"
-                                                     width="5" height="2" rx="1"></rect>
-                                                 <rect id="Rectangle-152-Copy" fill="#335EEA" opacity="0.3" x="7" y="14"
-                                                     width="9" height="2" rx="1"></rect>
-                                             </g>
-                                         </svg>
-                                     </div>
-
-                                     <!-- Content -->
-                                     <div class="ml-4">
-
-                                         <!-- Heading -->
-                                         <h6 class="font-weight-bold text-uppercase text-primary mb-0">
-                                             Documentation
-                                         </h6>
-
-                                         <!-- Text -->
-                                         <p class="font-size-sm text-gray-700 mb-0">
-                                             Customizing and building
-                                         </p>
-
-                                     </div>
-
-                                 </a>
-                                 <a class="list-group-item" href="docs/alerts.html">
-
-                                     <!-- Icon -->
-                                     <div class="icon icon-sm text-primary">
-                                         <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1"
-                                             xmlns="http://www.w3.org/2000/svg"
-                                             xmlns:xlink="http://www.w3.org/1999/xlink">
-                                             <!-- Generator: Sketch 52.2 (67145) - http://www.bohemiancoding.com/sketch -->
-                                             <title>Stockholm-icons / Layout / Layout-4-blocks</title>
-                                             <desc>Created with Sketch.</desc>
-                                             <g id="Stockholm-icons-/-Layout-/-Layout-4-blocks" stroke="none"
-                                                 stroke-width="1" fill="none" fill-rule="evenodd">
-                                                 <rect id="bound" x="0" y="0" width="24" height="24"></rect>
-                                                 <rect id="Rectangle-7" fill="#335EEA" x="4" y="4" width="7" height="7"
-                                                     rx="1.5">
-                                                 </rect>
-                                                 <path
-                                                     d="M5.5,13 L9.5,13 C10.3284271,13 11,13.6715729 11,14.5 L11,18.5 C11,19.3284271 10.3284271,20 9.5,20 L5.5,20 C4.67157288,20 4,19.3284271 4,18.5 L4,14.5 C4,13.6715729 4.67157288,13 5.5,13 Z M14.5,4 L18.5,4 C19.3284271,4 20,4.67157288 20,5.5 L20,9.5 C20,10.3284271 19.3284271,11 18.5,11 L14.5,11 C13.6715729,11 13,10.3284271 13,9.5 L13,5.5 C13,4.67157288 13.6715729,4 14.5,4 Z M14.5,13 L18.5,13 C19.3284271,13 20,13.6715729 20,14.5 L20,18.5 C20,19.3284271 19.3284271,20 18.5,20 L14.5,20 C13.6715729,20 13,19.3284271 13,18.5 L13,14.5 C13,13.6715729 13.6715729,13 14.5,13 Z"
-                                                     id="Combined-Shape" fill="#335EEA" opacity="0.3"></path>
-                                             </g>
-                                         </svg>
-                                     </div>
-
-                                     <!-- Content -->
-                                     <div class="ml-4">
-
-                                         <!-- Heading -->
-                                         <h6 class="font-weight-bold text-uppercase text-primary mb-0">
-                                             Components
-                                         </h6>
-
-                                         <!-- Text -->
-                                         <p class="font-size-sm text-gray-700 mb-0">
-                                             Full list of components
-                                         </p>
-
-                                     </div>
-
-                                 </a>
                                  <a class="list-group-item" href="{{ route('home') }}">
 
                                      <!-- Icon -->
@@ -175,15 +94,10 @@
 
                                          <!-- Text -->
                                          <p class="font-size-sm text-gray-700 mb-0">
-                                             Keep track of changes
+                                             Halo pustakawan, silahkan masuk!   
                                          </p>
 
-                                     </div>
-
-                                     <!-- Badge -->
-                                     <span class="badge badge-pill badge-primary-soft ml-auto">
-                                         1.1.0
-                                     </span>
+                                        </div>
 
                                  </a>
                              </div>
