ed683f28ec130069ac27068cf72e68205cde4a68
Elag <elangindra15@gmail.com>
2019-09-02 07:43:27 +0700

    fixed footer

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,186 +1,82 @@
  <!-- FOOTER
     ================================================== -->
-    <footer class="py-8 py-md-11 bg-gray-200">
-        <div class="container">
-          <div class="row">
-            <div class="col-12 col-md-4 col-lg-3">
-          
-              <!-- Brand -->
-              <img src="{{ asset('img/brand.svg') }}" alt="..." class="footer-brand img-fluid mb-2">
-  
-              <!-- Text -->
-              <p class="text-gray-700 mb-2">
-                A better way to build.
-              </p>
-  
-              <!-- Social -->
-              <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">
-                <li class="list-inline-item list-social-item mr-3">
-                  <a href="#!" class="text-decoration-none">
-                    <img src="{{ asset('img/icons/social/instagram.svg') }}" class="list-social-icon" alt="...">
-                  </a>
-                </li>
-                <li class="list-inline-item list-social-item mr-3">
-                  <a href="#!" class="text-decoration-none">
-                    <img src="{{ asset('img/icons/social/facebook.svg') }}" class="list-social-icon" alt="...">
-                  </a>
-                </li>
-                <li class="list-inline-item list-social-item mr-3">
-                  <a href="#!" class="text-decoration-none">
-                    <img src="{{ asset('img/icons/social/twitter.svg') }}" class="list-social-icon" alt="...">
-                  </a>
-                </li>
-                <li class="list-inline-item list-social-item">
-                  <a href="#!" class="text-decoration-none">
-                    <img src="{{ asset('img/icons/social/pinterest.svg') }}" class="list-social-icon" alt="...">
-                  </a>
-                </li>
-              </ul>
-  
-            </div>
-            <div class="col-6 col-md-4 col-lg-2">
-          
-              <!-- Heading -->
-              <h6 class="font-weight-bold text-uppercase text-gray-700">
-                Products
-              </h6>
-  
-              <!-- List -->
-              <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Page Builder
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    UI Kit
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Styleguide
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Documentation
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" class="text-reset">
-                    Changelog
-                  </a>
-                </li>
-              </ul>
-  
-            </div>
-            <div class="col-6 col-md-4 col-lg-2">
-          
-              <!-- Heading -->
-              <h6 class="font-weight-bold text-uppercase text-gray-700">
-                Services
-              </h6>
-  
-              <!-- List -->
-              <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Documentation
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Changelog
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Pagebuilder
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" class="text-reset">
-                    UI Kit
-                  </a>
-                </li>
-              </ul>
-  
-            </div>
-            <div class="col-6 col-md-4 offset-md-4 col-lg-2 offset-lg-0">
-          
-              <!-- Heading -->
-              <h6 class="font-weight-bold text-uppercase text-gray-700">
-                Connect
-              </h6>
-  
-              <!-- List -->
-              <ul class="list-unstyled text-muted mb-0">
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Page Builder
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    UI Kit
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Styleguide
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Documentation
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Changelog
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Documentation
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" class="text-reset">
-                    Changelog
-                  </a>
-                </li>
-              </ul>
-  
-            </div>
-            <div class="col-6 col-md-4 col-lg-2">
-          
-              <!-- Heading -->
-              <h6 class="font-weight-bold text-uppercase text-gray-700">
-                Legal
-              </h6>
-  
-              <!-- List -->
-              <ul class="list-unstyled text-muted mb-0">
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Documentation
-                  </a>
-                </li>
-                <li class="mb-3">
-                  <a href="#!" class="text-reset">
-                    Changelog
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" class="text-reset">
-                    Pagebuilder
-                  </a>
-                </li>
-              </ul>
-  
-            </div>
-          </div> <!-- / .row -->
-        </div> <!-- / .container -->
-      </footer>
+ <footer class="py-8 py-md-11 bg-gray-200">
+     <div class="container">
+         <div class="row">
+             <div class="col-12 col-md-4 col-lg-4">
+
+                 <!-- Brand -->
+                 <img src="{{ asset('img/brand.svg') }}" alt="..." class="footer-brand img-fluid mb-2">
+
+                 <!-- Text -->
+                 <p class="font-size-sm text-gray-700 mb-2">
+                  Email: info@stmik-amikbandung.ac.id
+                  <br>
+                  Telp:  (022) 7271136
+                </p>
+
+                 <!-- Social -->
+                 <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">
+                     <li class="list-inline-item list-social-item mr-3">
+                         <a href="#!" class="text-decoration-none">
+                             <img src="{{ asset('img/icons/social/instagram.svg') }}" class="list-social-icon"
+                                 alt="...">
+                         </a>
+                     </li>
+                     <li class="list-inline-item list-social-item mr-3">
+                         <a href="#!" class="text-decoration-none">
+                             <img src="{{ asset('img/icons/social/facebook.svg') }}" class="list-social-icon" alt="...">
+                         </a>
+                     </li>
+                     <li class="list-inline-item list-social-item mr-3">
+                         <a href="#!" class="text-decoration-none">
+                             <img src="{{ asset('img/icons/social/twitter.svg') }}" class="list-social-icon" alt="...">
+                         </a>
+                     </li>
+                 </ul>
+
+             </div>
+             <div class="col-6 col-md-4 col-lg-3">
+
+                 <!-- Heading -->
+                 <h6 class="font-weight-bold text-uppercase text-gray-700">
+                     Jadwal Perpustakaan
+                 </h6>
+
+                 <!-- List -->
+                 <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+                     <li class="mb-3">
+                         Senin - Jumat <br>
+                         <span class="font-size-sm">08.00 - 20.00</span>
+                     </li>
+                     <li class="mb-3">
+                      Sabtu <br>
+                      <span class="font-size-sm">08.00 - 17.00</span>
+                     </li>
+                     <li class="mb-3">
+                      Jam Istirahat <br>
+                      <span class="font-size-sm">12.00 - 13.00</span>
+                     </li>
+                 </ul>
+
+             </div>
+             <div class="col-6 col-md-4 col-lg-5">
+
+                 <!-- Heading -->
+                 <h6 class="font-weight-bold text-uppercase text-gray-700">
+                     Alamat
+                 </h6>
+
+                 <!-- List -->
+                 <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+                     <li class="mb-3">
+                         Jalan Jakarta No 28
+                         Kelurahan Kebonwaru, Kecamatan Batununggal
+                         Kota Bandung, Jawa barat
+                     </li>
+                 </ul>
+
+             </div>
+         </div> <!-- / .row -->
+     </div> <!-- / .container -->
+ </footer>
